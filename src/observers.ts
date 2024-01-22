f17fc5ceb86917259a51a7eec0b648c9231a2eb8
hayata-yamamoto <hayata.yamamoto.work@gmail.com>
2024-01-22 16:17:59 +0000

    prettier のフォーマット

--- src/observers.ts
+++ src/observers.ts
@@ -1,23 +1,22 @@
-
 export type PromiseResult = {
-    key: string;
-    responseTime: number;
-}
+  key: string;
+  responseTime: number;
+};
 export type ObserverType = {
-    onFullfilled: (result: PromiseResult) => Promise<void>;
-    onRejected: (result: PromiseResult) => Promise<void>;
-}
+  onFullfilled: (result: PromiseResult) => Promise<void>;
+  onRejected: (result: PromiseResult) => Promise<void>;
+};
 
 export const consoleObserver = (): ObserverType => {
-    const onFullfilled = async (result: PromiseResult) => {
-        console.log(`Fullfilled: ${result.key} in ${result.responseTime}ms`)
-    }
-    const onRejected = async (result: PromiseResult) => {
-        console.log(`Rejected: ${result.key} in ${result.responseTime}ms`)
-    }
+  const onFullfilled = async (result: PromiseResult) => {
+    console.log(`Fullfilled: ${result.key} in ${result.responseTime}ms`);
+  };
+  const onRejected = async (result: PromiseResult) => {
+    console.log(`Rejected: ${result.key} in ${result.responseTime}ms`);
+  };
 
-    return {
-        onFullfilled,
-        onRejected
-    }
-}
+  return {
+    onFullfilled,
+    onRejected,
+  };
+};
