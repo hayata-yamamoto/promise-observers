4e83647611ccebccd9e9fc9f2ca2c5b655a6ae3e
hayata-yamamoto <hayata.yamamoto.work@gmail.com>
2024-01-22 16:13:12 +0000

    Promise.all を使用する際にオブザーバーに対して通知を送れるようにする対応

---
+++ src/observers.ts
@@ -0,0 +1,23 @@
+
+export type PromiseResult = {
+    key: string;
+    responseTime: number;
+}
+export type ObserverType = {
+    onFullfilled: (result: PromiseResult) => Promise<void>;
+    onRejected: (result: PromiseResult) => Promise<void>;
+}
+
+export const consoleObserver = (): ObserverType => {
+    const onFullfilled = async (result: PromiseResult) => {
+        console.log(`Fullfilled: ${result.key} in ${result.responseTime}ms`)
+    }
+    const onRejected = async (result: PromiseResult) => {
+        console.log(`Rejected: ${result.key} in ${result.responseTime}ms`)
+    }
+
+    return {
+        onFullfilled,
+        onRejected
+    }
+}
