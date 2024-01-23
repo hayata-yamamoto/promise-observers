5bb214229c630ba098b7a4c336ea4b5e5e91f1f7
hayata-yamamoto <hayata.yamamoto.work@gmail.com>
2024-01-23 15:26:29 +0000

    made format

--- src/observers.ts
+++ src/observers.ts
@@ -1,22 +1,22 @@
 export type PromiseResult = {
-  key: string;
-  responseTime: number;
-};
+  key: string
+  responseTime: number
+}
 export type ObserverType = {
-  onFullfilled: (result: PromiseResult) => Promise<void>;
-  onRejected: (result: PromiseResult) => Promise<void>;
-};
+  onFullfilled: (result: PromiseResult) => Promise<void>
+  onRejected: (result: PromiseResult) => Promise<void>
+}
 
 export const consoleObserver = (): ObserverType => {
   const onFullfilled = async (result: PromiseResult): Promise<void> => {
-    console.log(`Fullfilled: ${result.key} in ${result.responseTime}ms`);
-  };
+    console.log(`Fullfilled: ${result.key} in ${result.responseTime}ms`)
+  }
   const onRejected = async (result: PromiseResult): Promise<void> => {
-    console.log(`Rejected: ${result.key} in ${result.responseTime}ms`);
-  };
+    console.log(`Rejected: ${result.key} in ${result.responseTime}ms`)
+  }
 
   return {
     onFullfilled,
-    onRejected,
-  };
-};
+    onRejected
+  }
+}
